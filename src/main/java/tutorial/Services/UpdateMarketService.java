7b3f2411be0385eaf7c82199663adbb1b0b073d5
Fábio Amarante <fabioamarante@hotmail.com>
2016-12-11 18:24:31 +0000

    Removed unused class

--- src/main/java/tutorial/Services/UpdateMarketService.java
+++
@@ -1,9 +0,0 @@
-package tutorial.Services;
-
-import jadex.commons.future.IFuture;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public interface UpdateMarketService {
-    public IFuture<Void> UpdateMarketService(ArrayList<HashMap> quote);
-}
